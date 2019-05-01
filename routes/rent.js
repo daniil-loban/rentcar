bd79e8d7e56f1cd4b01ad8f810d8f84d4148bd71
daniil-loban <dungeon_lda@mail.ru>
2019-05-01 11:32:01 +0300

    first commit

---
+++ routes/rent.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+//const fs = require('fs');
+
+router.post('/', function(req,res){
+  const carId = +req.body.carId;
+  const userData = req.body.userData;
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+  console.log(`Car Id = ${carId} start = ${startDate} end = ${endDate}, userData is ${JSON.stringify(userData)}`);
+  res.statusCode = 200;
+  res.end("OK");
+});
+
+module.exports = router;
